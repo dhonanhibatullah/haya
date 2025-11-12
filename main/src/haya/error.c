c15e0c25d7c45b383129fb141650a4f7a6956f0f
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2025-11-12 10:39:17 +0700

    fix: error strings moved to C to avoid multiple defintion

--- main/src/haya/error.c
+++ main/src/haya/error.c
@@ -1,5 +1,11 @@
 #include "haya/error.h"
 
+const char *_HY_ERR_STRINGS[] = {
+#define X(name) #name,
+    _HY_ERR_LIST
+#undef X
+};
+
 const char *hyErrToStr(HyErr err)
 {
     if (HY_ERR_NONE <= err && err < _HY_ERR_END)
