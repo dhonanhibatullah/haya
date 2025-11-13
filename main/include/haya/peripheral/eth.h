8e6c3f6ea7dc9c7d282ca329d5e7ccbc895ee3b5
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2025-11-13 16:01:58 +0700

    feat: eth setup for w5500 added

---
+++ main/include/haya/peripheral/eth.h
@@ -0,0 +1,20 @@
+#ifndef __HAYA_PERIPHERAL_ETH_H
+#define __HAYA_PERIPHERAL_ETH_H
+
+#include <string.h>
+#include "esp_err.h"
+#include "driver/spi_common.h"
+#include "esp_netif.h"
+#include "esp_eth.h"
+#include "esp_eth_mac_w5500.h"
+#include "esp_eth_phy_w5500.h"
+
+esp_err_t hyPeripheralEthW5500Setup(
+    esp_netif_t **netif,
+    spi_host_device_t spi_host,
+    int spi_clock_speed,
+    int cs_pin,
+    int int_pin,
+    int rst_pin);
+
+#endif
