501803a90d15b8984f5d669e48891bc279ddb0ca
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2025-11-17 13:28:09 +0700

    feat: WiFi config simplified

--- main/include/haya/peripheral/wifi.h
+++ main/include/haya/peripheral/wifi.h
@@ -5,14 +5,9 @@
 #include "esp_err.h"
 #include "esp_netif.h"
 #include "esp_wifi.h"
-#include "nvs_flash.h"
 
 esp_err_t hyPeripheralWiFiSetup(
-    nvs_handle_t nvs,
     esp_netif_t **ap_netif,
-    const char *ap_ssid,
-    const char *ap_pass,
-    int channel,
-    int max_connection);
+    esp_netif_t **sta_netif);
 
 #endif
--- main/src/haya/peripheral/wifi.c
+++ main/src/haya/peripheral/wifi.c
@@ -1,21 +1,9 @@
 #include "haya/peripheral/wifi.h"
 
 esp_err_t hyPeripheralWiFiSetup(
-    nvs_handle_t nvs,
     esp_netif_t **ap_netif,
-    const char *ap_ssid,
-    const char *ap_pass,
-    int channel,
-    int max_connection)
+    esp_netif_t **sta_netif)
 {
-    if (ap_netif == NULL || ap_ssid == NULL || ap_pass == NULL)
-        return ESP_ERR_INVALID_ARG;
-
-    size_t ssid_len = strlen(ap_ssid);
-    size_t pass_len = strlen(ap_pass);
-    if (ssid_len == 0 || ssid_len > 32 || pass_len > 64)
-        return ESP_ERR_INVALID_ARG;
-
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     esp_err_t err = esp_wifi_init(&cfg);
     if (err != ESP_OK)
@@ -28,85 +16,13 @@ esp_err_t hyPeripheralWiFiSetup(
         return ESP_FAIL;
     }
 
-    if (pass_len > 0 && pass_len < 8)
+    *sta_netif = esp_netif_create_default_wifi_sta();
+    if (*sta_netif == NULL)
     {
         esp_netif_destroy(*ap_netif);
-        esp_wifi_deinit();
         *ap_netif = NULL;
-        return ESP_ERR_INVALID_ARG;
-    }
-
-    wifi_config_t wifi_cfg = {
-        .ap = {
-            .channel = channel,
-            .max_connection = max_connection,
-            .authmode = WIFI_AUTH_OPEN,
-        }};
-
-    size_t saved_ssid_len = 32;
-    err = nvs_get_str(nvs, "wifi-ap-ssid", (char *)wifi_cfg.ap.ssid, &saved_ssid_len);
-    if (err == ESP_OK)
-    {
-        wifi_cfg.ap.ssid_len = saved_ssid_len - 1;
-    }
-    else
-    {
-        strncpy((char *)wifi_cfg.ap.ssid, ap_ssid, 32);
-        wifi_cfg.ap.ssid_len = ssid_len;
-        nvs_set_str(nvs, "wifi-ap-ssid", ap_ssid);
-    }
-
-    size_t saved_pass_len = 64;
-    err = nvs_get_str(nvs, "wifi-ap-pass", (char *)wifi_cfg.ap.password, &saved_pass_len);
-
-    if (err == ESP_OK)
-    {
-        if (saved_pass_len > 1)
-            wifi_cfg.ap.authmode = WIFI_AUTH_WPA2_PSK;
-    }
-    else
-    {
-        if (pass_len >= 8)
-        {
-            wifi_cfg.ap.authmode = WIFI_AUTH_WPA2_PSK;
-            strncpy((char *)wifi_cfg.ap.password, ap_pass, 64);
-            nvs_set_str(nvs, "wifi-ap-pass", ap_pass);
-        }
-        else
-        {
-            nvs_set_str(nvs, "wifi-ap-pass", "");
-        }
-    }
-
-    nvs_commit(nvs);
-
-    err = esp_wifi_set_mode(WIFI_MODE_AP);
-    if (err != ESP_OK)
-    {
-        esp_netif_destroy(*ap_netif);
         esp_wifi_deinit();
-        *ap_netif = NULL;
-        return err;
-    }
-
-    err = esp_wifi_set_config(WIFI_IF_AP, &wifi_cfg);
-    if (err != ESP_OK)
-    {
-        esp_wifi_set_mode(WIFI_MODE_NULL);
-        esp_netif_destroy(*ap_netif);
-        esp_wifi_deinit();
-        *ap_netif = NULL;
-        return err;
-    }
-
-    err = esp_wifi_start();
-    if (err != ESP_OK)
-    {
-        esp_wifi_set_mode(WIFI_MODE_NULL);
-        esp_netif_destroy(*ap_netif);
-        esp_wifi_deinit();
-        *ap_netif = NULL;
-        return err;
+        return ESP_FAIL;
     }
 
     return ESP_OK;
