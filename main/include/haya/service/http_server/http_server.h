84dabee5c719085d4f7c08dc5c9fd5bad14b7f76
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2025-11-19 20:40:01 +0700

    refactor: whole server initiated inside http_server service

--- main/include/haya/service/http_server/http_server.h
+++ main/include/haya/service/http_server/http_server.h
@@ -7,7 +7,7 @@
 extern const uint8_t _404_html_start[] asm("_binary_404_min_html_start");
 extern const uint8_t _404_html_end[] asm("_binary_404_min_html_end");
 
-esp_err_t hyHttpServerSetup(httpd_handle_t server);
+esp_err_t hyHttpServerSetup(httpd_handle_t *server, uint16_t port);
 
 esp_err_t _hyHttpServer404(httpd_req_t *req, httpd_err_code_t err);
 
--- main/src/haya/service/http_server/http_server.c
+++ main/src/haya/service/http_server/http_server.c
@@ -1,10 +1,20 @@
 #include "haya/service/http_server/http_server.h"
 
-esp_err_t hyHttpServerSetup(httpd_handle_t server)
+esp_err_t hyHttpServerSetup(httpd_handle_t *server, uint16_t port)
 {
-    esp_err_t err = httpd_register_err_handler(server, HTTPD_404_NOT_FOUND, _hyHttpServer404);
+    httpd_config_t httpd_config = HTTPD_DEFAULT_CONFIG();
+    httpd_config.server_port = port;
+
+    esp_err_t err = httpd_start(server, &httpd_config);
     if (err != ESP_OK)
         return err;
+
+    err = httpd_register_err_handler(*server, HTTPD_404_NOT_FOUND, _hyHttpServer404);
+    if (err != ESP_OK)
+    {
+        httpd_stop(*server);
+        return err;
+    }
 
     return ESP_OK;
 }
