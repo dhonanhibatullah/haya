7fa454fa0f743601149a4352dde8fdf78908195e
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2026-05-15 10:40:48 +0700

    refactor/total: (refactor) Cleaning up.

--- main/include/haya/service/wifiman/html.h
+++
@@ -1,9 +0,0 @@
-#ifndef __WIFIMAN_HTML_H
-#define __WIFIMAN_HTML_H
-
-#include <stdint.h>
-
-extern const uint8_t index_html_start[] asm("_binary_wifiman_min_html_start");
-extern const uint8_t index_html_end[] asm("_binary_wifiman_min_html_end");
-
-#endif
--- main/include/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MAIN_H
-#define __MAIN_H
-
-#include "core/core.h"
-
-void app_main();
-
-#endif
